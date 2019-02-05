1832c98adf36e3feb725208a7d0f142099df5c2d
aamayorga <amayorga95@outlook.com>
2019-02-05 13:24:00 -0800

    Initial Commit

---
+++ KrasamoStockTrackerTests/KrasamoStockTrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  KrasamoStockTrackerTests.swift
+//  KrasamoStockTrackerTests
+//
+//  Created by Antonio Mayorga on 2/5/19.
+//  Copyright © 2019 Rays Industrial Computers. All rights reserved.
+//
+
+import XCTest
+@testable import KrasamoStockTracker
+
+class KrasamoStockTrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
