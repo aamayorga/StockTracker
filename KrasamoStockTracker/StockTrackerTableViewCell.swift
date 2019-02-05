8ae8796e566b08fc2c2c8917e5e766b1aaa84c94
aamayorga <amayorga95@outlook.com>
2019-02-05 15:30:23 -0800

    Added styling to stock tracker table view cells

---
+++ KrasamoStockTracker/StockTrackerTableViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  StockTrackerTableViewCell.swift
+//  KrasamoStockTracker
+//
+//  Created by Antonio Mayorga on 2/5/19.
+//  Copyright © 2019 Rays Industrial Computers. All rights reserved.
+//
+
+import UIKit
+
+class StockTrackerTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var stockSymbol: UILabel!
+    @IBOutlet weak var currentStockPrice: UILabel!
+    @IBOutlet weak var maximumStockPrice: UILabel!
+    @IBOutlet weak var minimumStockPrice: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let fontSize: CGFloat = 14.0
+        let fontColor = UIColor.white
+        
+        stockSymbol.font = UIFont.systemFont(ofSize: fontSize)
+        stockSymbol.textColor = fontColor
+        
+        currentStockPrice.font = UIFont.systemFont(ofSize: fontSize)
+        currentStockPrice.textColor = fontColor
+        
+        maximumStockPrice.font = UIFont.systemFont(ofSize: fontSize)
+        maximumStockPrice.textColor = fontColor
+        
+        minimumStockPrice.font = UIFont.systemFont(ofSize: fontSize)
+        minimumStockPrice.textColor = fontColor
+        
+        self.backgroundColor = UIColor.init(displayP3Red: 35.0/255.0, green: 31.0/255.0, blue: 32.0/255.0, alpha: 1.0)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
