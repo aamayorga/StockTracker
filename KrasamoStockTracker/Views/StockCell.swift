db5b37b30cf784bd0f62ab1e2cacff9885395d3b
aamayorga <amayorga95@outlook.com>
2019-02-06 04:13:38 -0800

    Adjusted font and table view cell seperators

--- KrasamoStockTracker/Views/StockCell.swift
+++ KrasamoStockTracker/Views/StockCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class StockTrackerTableViewCell: UITableViewCell {
+class StockCell: UITableViewCell {
 
     @IBOutlet weak var stockSymbol: UILabel!
     @IBOutlet weak var currentStockPrice: UILabel!
@@ -21,17 +21,20 @@ class StockTrackerTableViewCell: UITableViewCell {
         let fontSize: CGFloat = 14.0
         let fontColor = UIColor.white
         
-        stockSymbol.font = UIFont.systemFont(ofSize: fontSize)
+        stockSymbol.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
         stockSymbol.textColor = fontColor
         
-        currentStockPrice.font = UIFont.systemFont(ofSize: fontSize)
+        currentStockPrice.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
         currentStockPrice.textColor = fontColor
         
-        maximumStockPrice.font = UIFont.systemFont(ofSize: fontSize)
+        maximumStockPrice.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
         maximumStockPrice.textColor = fontColor
         
-        minimumStockPrice.font = UIFont.systemFont(ofSize: fontSize)
+        minimumStockPrice.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
         minimumStockPrice.textColor = fontColor
+        
+        separatorInset.left = 16
+        separatorInset.right = 16
         
         self.backgroundColor = UIColor.init(displayP3Red: 35.0/255.0, green: 31.0/255.0, blue: 32.0/255.0, alpha: 1.0)
     }
