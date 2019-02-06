375094298b9f8ee3c68f14164d7a5c9135da20a6
aamayorga <amayorga95@outlook.com>
2019-02-06 00:25:14 -0800

    Add Quote network request from IEX

--- KrasamoStockTracker/Network/IEXClient.swift
+++ KrasamoStockTracker/Network/IEXClient.swift
@@ -9,6 +9,46 @@
 import Foundation
 
 class IEXClient {
+    
+    func getQuote(ForSymbol symbol: String, completion: @escaping(_ quote: Quote?, _ error: Error?) -> Void) {
+        
+        func sendError(_ error: String) {
+            print(error)
+            let userInfo = [NSLocalizedDescriptionKey: error]
+            completion(nil, NSError(domain: "getQuote", code: 1, userInfo: userInfo))
+        }
+        
+        let request = [IEXConstants.IEXRequest.Stock, "/\(symbol.lowercased())", IEXConstants.IEXRequest.Quote]
+        
+        guard let url = iexUrlWithUrlRequests(request) else {
+            sendError("Invalid URL")
+            return
+        }
+        
+        let urlTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard error == nil else {
+                sendError("Could not complete request. Check internet connection and try again.")
+                return
+            }
+            
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("No company symbol found.")
+                return
+            }
+            
+            guard let data = data else {
+                sendError("No stock data found.")
+                return
+            }
+            
+            print(data)
+        }
+        
+        urlTask.resume()
+    }
+    
+    
+    
     func iexUrlWithUrlRequests(_ requests: [String]) -> URL? {
         var components = URLComponents()
         components.scheme = IEXConstants.IEXURLComponents.ApiScheme
