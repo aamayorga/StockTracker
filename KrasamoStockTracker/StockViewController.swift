3ac8f418fd2fab69a206323aa811bad56ea84216
aamayorga <amayorga95@outlook.com>
2019-02-06 04:25:29 -0800

    Set up delegate methods for Table View and Text View

--- KrasamoStockTracker/StockViewController.swift
+++ KrasamoStockTracker/StockViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class StockViewController: UIViewController {
 
+    let CELL_REUSE_IDENTIFIER = "stockCell"
+    let USER_DEFAULT_KEY      = "stockUserDefaultsArray"
+    
     @IBOutlet weak var stockTableView: UITableView!
     @IBOutlet weak var stockSymbolPopupView: UIView!
     @IBOutlet weak var dismissPopupButton: UIButton!
@@ -18,9 +21,23 @@ class StockViewController: UIViewController {
     @IBOutlet weak var stockPopupTextField: UITextField!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    var stockQuotes: [Quote] = [] {
+        didSet {
+            stockTableView.reloadData()
+            let stockSymbols = stockQuotes.map({ return $0.symbol })
+            UserDefaults.standard.set(stockSymbols, forKey: USER_DEFAULT_KEY)
+        }
+    }
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         
         let darkGray = UIColor.init(displayP3Red: 35.0/255.0, green: 31.0/255.0, blue: 32.0/255.0, alpha: 1.0)
+        
+        stockTableView.delegate = self
+        stockTableView.dataSource = self
+        stockTableView.backgroundColor = darkGray
+        stockTableView.tableFooterView = UIView()
         
         stockSymbolPopupView.alpha = 0.0
         dismissPopupButton.isEnabled = false
@@ -39,5 +56,31 @@ class StockViewController: UIViewController {
         
         view.backgroundColor = darkGray
         
+    }
+}
+
+extension StockViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stockQuotes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_REUSE_IDENTIFIER, for: indexPath) as! StockCell
+        
+        cell.stockSymbol.text = stockQuotes[indexPath.row].symbol
+        cell.maximumStockPrice.text = "\(stockQuotes[indexPath.row].high ?? 0.0)"
+        cell.minimumStockPrice.text = "\(stockQuotes[indexPath.row].low ?? 0.0)"
+        cell.currentStockPrice.text = "\(stockQuotes[indexPath.row].latestPrice ?? 0.0)"
+        
+        return cell
+    }
+}
+
+extension StockViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
     }
 }
