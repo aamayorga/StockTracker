be5e81b6b455a5eb26e85c9ba11fa44009bc633b
aamayorga <amayorga95@outlook.com>
2019-02-06 00:17:17 -0800

    Made a file with the necessary URL components to construct my GET request for the IEX API

---
+++ KrasamoStockTracker/Network/IEXConstants.swift
@@ -0,0 +1,24 @@
+//
+//  IEXConstants.swift
+//  KrasamoStockTracker
+//
+//  Created by Antonio Mayorga on 2/5/19.
+//  Copyright © 2019 Rays Industrial Computers. All rights reserved.
+//
+
+import Foundation
+
+class IEXConstants {
+    
+    struct IEXURLComponents {
+        static let ApiScheme    = "https"
+        static let ApiHost      = "api.iextrading.com"
+        static let ApiPath      = "/1.0"
+    }
+    
+    struct IEXRequest {
+        static let Stock        = "/stock"
+        static let Quote        = "/quote"
+        static let Price        = "/price"
+    }
+}
