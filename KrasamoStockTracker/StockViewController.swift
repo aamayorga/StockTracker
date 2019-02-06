ab182bb0b57f0667ee7bce70d18c53ddbcaa9227
aamayorga <amayorga95@outlook.com>
2019-02-06 04:17:41 -0800

    Added custom class for view controller

---
+++ KrasamoStockTracker/StockViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StockViewController.swift
+//  KrasamoStockTracker
+//
+//  Created by Antonio Mayorga on 2/5/19.
+//  Copyright © 2019 Rays Industrial Computers. All rights reserved.
+//
+
+import UIKit
+
+class StockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+    }
+}
